321397605f3663178f13c8d74f62e2ad1b704d56
Nikola Goranovic <nikola.goranovicvg@gmail.com>
2021-10-25 19:51:07 +0200

    Add in memory security fod Doodle application

--- src/main/java/com/tx/doodle/DoodleApplication.java
+++ src/main/java/com/tx/doodle/DoodleApplication.java
@@ -3,11 +3,12 @@ package com.tx.doodle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
 @EntityScan("com.tx.doodle.model")
 @EnableMongoRepositories(basePackages = "com.tx.doodle.repository")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DoodleApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/tx/doodle/config/DoodleSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.tx.doodle.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class DoodleSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic()
+        .and()
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("stark")
+        .password(passwordEncoder().encode("iamironman"))
+        .roles("USER");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
